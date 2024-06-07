aff00a175768ac8509c7db8cd4196b80bcce7c39
muh11 <naufalaswinana@gmail.com>
2024-06-07 14:51:51 +0700

    suhu component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-import './App.css'
-
+import React from "react";
+import "./App.css";
+import Suhu from "./components/Suhu";
 
 function App() {
-  return {
-    <>
-    hello world 
-    <>
-
-  }
+  return (
+    <div className="bg-[#1F213A] h-screen flex justify-center align-top ">
+      <div className="bg-blue-200 mt-40 w-1/5 h-1/3 ">
+        <Suhu />{" "}
+      </div>
+      <div className="bg-blue-400 mt-40 w-1/3 h-1/3"> right </div>
+    </div>
+  );
 }
 
-
-export default App
+export default App;
