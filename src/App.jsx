673fcebbd1124cdae928e0a638c9930aaec14762
muh11 <naufalaswinana@gmail.com>
2024-06-07 16:08:54 +0700

    fetching apiURL

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Suhu from "./components/Suhu";
+import Highlights from "./components/Highlights";
 
 function App() {
+  const {city, setCity} = useState("Depok");
+  const [weatherData, setWeatherData] = [useState(null)];
+
+  const apirURL = `https://api.weatherapi.com/v1/current.json?key=e57e4269f7054422a0f85241240706&q=${city}&aqi=no`
+
+  useEffect(()=>{
+  fetch(apirURL)
+  .then((response)=>{
+    if(!response.ok){
+      throw new Error("Error");
+    }
+    return response.json();
+  })
+  .then((data)=>{
+    console.log(data);
+    setWeatherData(data);
+  })
+  .catch((e)=>{
+    console.log(e);
+  })
+},[city])
+
   return (
     <div className="bg-[#1F213A] h-screen flex justify-center align-top ">
-      <div className="bg-blue-200 mt-40 w-1/5 h-1/3 ">
+      <div className=" mt-40 w-1/5 h-1/3 ">
         <Suhu />{" "}
       </div>
-      <div className="bg-blue-400 mt-40 w-1/3 h-1/3"> right </div>
+
+
+      <div className="mt-40 w-1/3 h-1/3 p-10 grid grid-cols-2 gap-6"> 
+      <h2 className="text-slate-200 text-2xl col-span-2
+      ">Today's Highlight</h2>
+      <Highlights />
+      <Highlights />
+      <Highlights />
+      <Highlights />
+    </div>
     </div>
   );
 }
